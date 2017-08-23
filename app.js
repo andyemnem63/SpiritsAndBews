78f5dceeb10a54747d60c528e2873baf17240af3
Goober <balabaninteractive@gmail.com>
2017-08-23 14:07:56 -0400

    set up

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ var port = process.env.PORT || 8000;
 
 var pug = require('pug');
 var mysql = require('mysql');
-var md5 = require('MD5');
+var md5 = require('md5');
 var bodyParser = require('body-parser');
 
 var app = express();
 
 app.set('view engine', 'pug');
 
- app.get('/', function(req, res) {
- 	res.render('index');
- });
+app.use(express.static('public/img/page-elements'));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
 
 app.listen(port, function(){
 	console.log("server on: " + port);
